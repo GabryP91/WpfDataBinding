c840ff8ad783ed640ce6711d6d94ca665ab01ffe
GabryP91 <gabry.palma@libero.it>
2025-04-20 10:35:01 +0200

    Aggiungere i file di progetto.

---
+++ WpfDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WpfDataBinding
+{
+    
+    public partial class MainWindow : Window , INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set {
+
+                boundText = value;
+                OnpropertyChange();
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code";
+        }
+
+        private void OnpropertyChange([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
